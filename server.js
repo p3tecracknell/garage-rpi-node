ceba23ccc4f4c6a560fc46f3dfe328f19059c4dd
Pete <p3tecracknell@gmail.com>
2019-09-12 22:17:41 +0100

    Tweak UI

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const app = express()
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+
 const gpio = require('./gpio')
 
 const PORT = process.env.PORT || 3000
-
-app.use(express.static('web'))
-app.use(bodyParser.urlencoded())
 
 app.post('/action', async (req, res) => {
     const pin = req.body.pin
@@ -16,10 +16,11 @@ app.post('/action', async (req, res) => {
     res.redirect('/')
 })
 
+app.post('/fulfillment', app)
+
 gpio.setup([7, 8])
     .then(async () => {
-        app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
-
+      app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
     })
     .catch((err) => {
         console.log('Error: ', err.toString())
