978d27fbcd2e6fcf4fca87db5b6d38d94ea96c8d
Pete <p3tecracknell@gmail.com>
2019-09-20 20:56:42 +0100

    working on clean up

--- gpio.js
+++ gpio.js
@@ -19,15 +19,23 @@ if (!PROD) {
 
 const sleep = require('util').promisify(setTimeout)
 
+const setupPins = []
 module.exports = {
     setup: async (pins) => {
         for (let pin of pins) {
             await gpiop.setup(pin, gpio.DIR_OUT)
+            setupPins.push(pin)
         }
     },
     run: async (pin) => {
         await gpiop.write(pin, true)
         await sleep(500)
         await gpiop.write(pin, false)
+    },
+    cleanup: async () => {
+        for (let pin of setupPins) {
+            await gpiop.setup(pin, gpio.DIR_IN)
+            console.log(`Set ${pin} back to IN`)
+        }
     }
 }
--- server.js
+++ server.js
@@ -10,13 +10,13 @@ const gpio = require('./gpio')
 
 const PORT = process.env.PORT || 3000
 
+app.use(express.static('web'))
+
 app.post('/action', async (req, res) => {
     const pin = req.body.pin
     await gpio.run(pin)
     res.redirect('/')
 })
-
-app.post('/fulfillment', app)
 
 gpio.setup([7, 8])
     .then(async () => {
@@ -25,3 +25,23 @@ gpio.setup([7, 8])
     .catch((err) => {
         console.log('Error: ', err.toString())
     })
+
+async function exitHandler(options, exitCode) {
+    await gpio.cleanup()
+    if (options.cleanup) console.log('clean');
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
